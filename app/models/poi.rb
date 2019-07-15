336cf5c7549034cdf4ccabe97a582a1eebfdec9c
wipegup <wipegup@gmail.com>
2019-07-15 16:55:59 -0600

    Adds validations for new pois columns

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -2,7 +2,8 @@ class Poi < ApplicationRecord
   has_many :trip_pois
   has_many :trips, through: :trip_pois
 
-  validates_presence_of :name,
+  validates_presence_of :name, :population, :state,
+                        :land_area, :total_area,
                         :ne_latitude, :ne_longitude,
                         :sw_latitude, :sw_longitude
 end
