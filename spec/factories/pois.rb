6a260e1cb3b7cdfe0e75c471c8f8df4813425372
wipegup <wipegup@gmail.com>
2019-07-16 07:15:29 -0600

    Add attributes to poi factory

--- spec/factories/pois.rb
+++ spec/factories/pois.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     ne_longitude { 1.5 }
     sw_latitude { 1.5 }
     sw_longitude { 1.5 }
+    population { 1000 }
+    state { "CO" }
+    land_area { "2500" }
+    total_area { "3000" }
   end
 end
