5127b7b0b825d06387ee03a7e9d5ae33d8e1fabf
wipegup <wipegup@gmail.com>
2019-07-17 13:50:25 -0600

    Reformat trip_facade for new POI serializer

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -6,61 +6,36 @@ class TripFacade
     @destination = trip_params[:destination]
   end
 
-  ### This needs to be updated
-  
   def response
-    # Origin / destination names from Google Maps
-    start_address = _google_maps_service.start_address
-    end_address = _google_maps_service.end_address
+    origin = _poi_service.places[0]
+    origin_weather = ForecastSerializer.new(_weather_service.get(origin.center.lat, origin.center.lng)).currently
+    places = [PoiSerializer.new(origin, origin_weather).to_json]
+    legs = []
+    running_time = 0.0
+    _poi_service.places[1..-1].zip(_poi_service.legs).each_with_index do |(place, leg), index|
+      running_time += leg.time
+      place_weather = ForecastSerializer.new(_weather_service.get(place.center.lat, place.center.lng)).hourly_in(running_time.round)
 
-    # Get long/lat of origin and destination
-    origin_lat = _google_maps_service.origin_lat
-    origin_lng = _google_maps_service.origin_lng
-    destination_lat = _google_maps_service.destination_lat
-    destination_lng = _google_maps_service.destination_lng
-
-    # Get miles and time between origin and destination
-    distance_text = _google_maps_service.distance_text
-    duration_hours = _google_maps_service.duration_hours
-
-    # Get population or origin and destination
-    origin_population = Poi.population_at_location(origin_lat, origin_lng)
-    destination_population = Poi.population_at_location(destination_lat, destination_lng)
-
-    # Get weather of origin currently
-    origin_weather = ForecastSerializer.new(_weather_service.get(origin_lat, origin_lng)).currently
-
-    # Get weather of destination at X hours from now
-    destination_weather = ForecastSerializer.new(_weather_service.get(destination_lat, destination_lng)).hourly_in(duration_hours.round)
+      places << PoiSerializer.new(place, place_weather).to_json
+      legs << leg.json_with_id(index)
+    end
 
     return {
             data: {
-                places: [
-                    {
-                        "name": start_address,
-                        "population": origin_population,
-                        "weather": origin_weather,
-                    },
-                    {
-                        "name": end_address,
-                        "population": destination_population,
-                        "weather": destination_weather
-                    }
-                ],
-                legs: [
-                    {
-                        "distance": distance_text,
-                        "duration_in_hours": duration_hours
-                    }
-                ]
+                places: places,
+                legs: legs
               }
             }
   end
 
   private
 
-    def _google_maps_service
-      @_google_maps_service  ||= GoogleMapsService.new(origin, destination)
+    def _poi_service
+      @_poi_service ||= PoiService.new(_directions.steps)
+    end
+
+    def _directions
+      @_directions  ||= GoogleMapsService.new(origin, destination)
     end
 
     def _weather_service
