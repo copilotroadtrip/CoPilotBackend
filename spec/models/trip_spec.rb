3885b4339f17ac1a5af9c2a24bdfe95c11b6155f
wipegup <wipegup@gmail.com>
2019-07-17 17:49:39 -0600

    Add tests for setting status and token creation

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Trip, type: :model do
   describe 'Validations' do
-    it { should validate_presence_of :token }
+    it { should define_enum_for :status }
   end
 
   describe 'Relationships' do
     it { should have_many :trip_pois }
     it { should have_many(:pois).through(:trip_pois) }
+  end
+
+  it 'Trip creation generates token, sets status' do
+    trip = Trip.create
+    expect(trip.token).not_to eq(nil)
+    expect(trop.status).to eq("pending")
   end
 end
