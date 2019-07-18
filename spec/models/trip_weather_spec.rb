b75b2b98e66f474b3decb4786eb62f2183c46415
wipegup <wipegup@gmail.com>
2019-07-17 18:14:01 -0600

    Add validations and relationships to tripweather test

--- spec/models/trip_weather_spec.rb
+++ spec/models/trip_weather_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe TripWeather, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it { should validate_presence_of :time }
+    it { should validate_presence_of :summary }
+    it { should validate_presence_of :temperature }
+    it { should validate_presence_of :precipProbability }
+    it { should validate_presence_of :precipIntensity }
+    it { should validate_presence_of :windSpeed }
+    it { should validate_presence_of :windGust }
+    it { should validate_presence_of :windBearing }
+    it { should validate_presence_of :icon }
+  end
+
+  describe 'Relationships' do
+    it {should belong_to :trip }
+  end
 end
