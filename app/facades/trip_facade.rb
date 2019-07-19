9ec3bf4c9c9440c94c2fdb13563b32c150a3ad7a
Matt Levy <milevy1@gmail.com>
2019-07-19 07:35:17 -0600

    Updates TripFacade to return only origin and destination data

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,31 +1,70 @@
 class TripFacade
-  attr_reader :origin, :destination
+  attr_reader :origin, :destination, :trip
 
   def initialize(trip_params)
     @origin = trip_params[:origin]
     @destination = trip_params[:destination]
+    @trip = Trip.create
+
+    # Add SidekiqWorker job here to build POIs and legs of trip
+    # The worker will need the trip id/token and Google directions payload
+    # Otherwise it will need to make a second Google API request
   end
 
   def response
-    origin = _poi_service.places[0]
-    origin_weather = ForecastSerializer.new(_weather_service.get(origin.center.lat, origin.center.lng)).currently
-    places = [PoiSerializer.new(origin, origin_weather).to_json]
-    legs = []
-    running_time = 0.0
-    _poi_service.places[1..-1].zip(_poi_service.legs).each_with_index do |(place, leg), index|
-      running_time += leg.time
-      place_weather = ForecastSerializer.new(_weather_service.get(place.center.lat, place.center.lng)).hourly_in(running_time.round)
-
-      places << PoiSerializer.new(place, place_weather).to_json
-      legs << leg.json_with_id(index)
-    end
-
     return {
-            data: {
-                places: places,
-                legs: legs
-              }
-            }
+      data: {
+        trip_token: trip.token,
+        places: [origin_data, destination_data],
+        legs: [legs_data]
+      }
+    }
+  end
+
+  def origin_data
+    {
+       "start_location": {
+           "lat": _directions.start_lat,
+           "lng": _directions.start_lng
+       },
+       "name": _directions.start_address,
+       "state": Poi.poi_at_location(_directions.start_lat, _directions.start_lng)[0].state,
+       "population": Poi.population_at_location(_directions.start_lat, _directions.start_lng),
+       "weather": origin_weather
+   }
+  end
+
+  def origin_weather
+    weather_data = _weather_service.get(_directions.start_lat, _directions.start_lng)
+
+    ForecastSerializer.new(weather_data).currently
+  end
+
+  def destination_data
+    {
+       "end_location": {
+           "lat": _directions.end_lat,
+           "lng": _directions.end_lng
+       },
+       "name": _directions.end_address,
+       "state": Poi.poi_at_location(_directions.end_lat, _directions.end_lng)[0].state,
+       "population": Poi.population_at_location(_directions.end_lat, _directions.end_lng),
+       "weather": destination_weather
+   }
+  end
+
+  def destination_weather
+    weather_data = _weather_service.get(_directions.end_lat, _directions.end_lng)
+    duration_hours = _directions.duration_hours
+    # Get weather of destination at X hours from now
+    ForecastSerializer.new(weather_data).hourly_in(duration_hours.round)
+  end
+
+  def legs_data
+    {
+      "distance": _directions.distance_text,
+      "duration_in_hours": _directions.duration_hours
+    }
   end
 
   private
@@ -41,6 +80,4 @@ class TripFacade
     def _weather_service
       @_weather_service_origin ||= DarkSkyService.new()
     end
-
-
 end
