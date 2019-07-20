648782ea9ea964d6ca23cb213de2ce314acdb873
Matt Levy <milevy1@gmail.com>
2019-07-20 15:06:32 -0600

    Adds sad path spec for invalid token for GET api/v1/trips

---
+++ spec/requests/api/v1/get_trips_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Trips API V1 requests', type: :request do
+  describe 'GET /api/v1/trips' do
+    before :each do
+      @trip = Trip.create
+      @valid_params = { token: @trip.token }
+    end
+
+    it 'status 404 if an invalid trip token provided' do
+      get '/api/v1/trips/', params: { token: "1234" }
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      body = JSON.parse(response.body)
+
+      expect(body['message']).to eq('Invalid Token')
+    end
+
+    xit 'returns a successful response' do
+      get '/api/v1/trips/', params: @valid_params
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+    end
+  end
+end
