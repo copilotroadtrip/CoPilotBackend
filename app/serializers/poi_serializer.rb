f7259ea8ae82c301563939a40dd120eecb61b481
wipegup <wipegup@gmail.com>
2019-07-19 20:19:04 -0600

    Add weather to POI serialization

--- app/serializers/poi_serializer.rb
+++ app/serializers/poi_serializer.rb
@@ -1,19 +1,39 @@
 class PoiSerializer
-  attr_reader :name, :population, :weather, :id
+  attr_reader :name, :population, :weather, :id, :state, :location
 
-  def initialize(poi, forecast)
+  def initialize(poi)
+    # binding.pry
     @id = poi.id
+    @location = poi.center
     @name = poi.name
     @population = poi.population
-    @weather = forecast
+    @state = poi.state
+    @weather = get_forecast
   end
 
   def to_json
     {
-      id: @id,
-      name: @name,
-      population: @population,
-      weather: @weather,
+      id: id,
+      location: location,
+      name: name,
+      state: state,
+      population: population,
+      weather: weather,
     }
+  end
+
+  def to_json_with_timing(hours_from_now)
+    forecast = ForecastSerializer.new(to_json[:weather]).hourly_in(hours_from_now)
+    to_json.merge({weather: forecast})
+  end
+
+  private
+
+  def get_forecast
+    _weather_service.get(location)
+  end
+
+  def _weather_service
+    @_weather_service ||= DarkSkyService.new
   end
 end
