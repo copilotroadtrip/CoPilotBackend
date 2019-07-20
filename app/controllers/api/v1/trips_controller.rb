0d71816cb8da65ee3f32e8a1b8b88e36d8102dac
Matt Levy <milevy1@gmail.com>
2019-07-20 15:06:57 -0600

    Adds sad path check for invalid token to GET api/v1/trips controller

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -3,8 +3,31 @@ class Api::V1::TripsController < ActionController::API
     render json: TripFacade.new(trip_params).response, status: 201
   end
 
+  def index
+    # Lookup trip by token
+    trip = Trip.find_by(token: trip_token_params[:token])
+
+    # If no trip exists, invalid trip_token
+    unless trip
+      render json: { message: 'Invalid Token'}, status: 404
+    end
+
+    # else... check if trip.status == "Pending"
+      # Pending - Return Status 200 / JSON body of:
+      # {"message": "trip pending"}
+
+    # If valid trip_token AND trip.status == "Ready"
+    # return all POIs and Legs
+
+
+  end
+
   private
     def trip_params
       params.permit(:origin, :destination)
+    end
+
+    def trip_token_params
+      params.permit(:token)
     end
 end
