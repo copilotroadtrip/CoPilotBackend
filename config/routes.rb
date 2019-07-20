4315b91bcc75bde92ebd9a2c67cd457c52d8c391
Matt Levy <milevy1@gmail.com>
2019-07-20 15:06:00 -0600

    Adds Trips#index to route to build GET api/v1/trips endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :trips, only: [:create]
+      resources :trips, only: [:create, :index]
     end
   end
 end
