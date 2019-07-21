e5868b4fb6cfbfa2cfe777c3500a565db5f487e3
Matt Levy <milevy1@gmail.com>
2019-07-21 13:03:27 -0600

    Refactored TripController index if statements

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -8,17 +8,15 @@ class Api::V1::TripsController < ActionController::API
     trip = Trip.find_by(token: trip_token_params[:token])
 
     # If no trip exists, invalid trip_token
-    unless trip
+    if !trip
       render json: { message: 'Invalid Token'}, status: 404
-    end
 
     # If valid trip AND trip.status == 'pending', POI data is not yet ready
-    if trip && trip.pending?
+    elsif trip.pending?
       render json: { message: 'Trip pending'}, status: 202
-    end
 
     # If valid trip AND trip.status == "Ready", return all POIs and Legs
-    if trip && trip.ready?
+    elsif trip.ready?
       render json: TripLegsFacade.new(trip).response, status: 200
     end
   end
