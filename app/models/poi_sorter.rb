f8f29b0a1457711d5b0240b16e3346dc6eb0e731
wipegup <wipegup@gmail.com>
2019-07-21 21:35:25 -0600

    Add poi_sorter class - helps poi_collection

---
+++ app/models/poi_sorter.rb
@@ -0,0 +1,14 @@
+class PoiSorter
+  attr_reader :keep, :discard
+  def initialize(poi_list)
+    @keep, @discard = sort_poi(poi_list)
+  end
+
+  def sort_poi(poi_list)
+    keep = poi_list.order(population:"DESC").first
+    discard = poi_list.where.not(id: keep.id)
+
+    return [keep, discard]
+  end
+
+end
