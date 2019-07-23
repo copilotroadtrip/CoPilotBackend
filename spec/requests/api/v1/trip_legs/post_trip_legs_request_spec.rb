150989c9ec8088d202fe6a0881483492f15b9036
Matt Levy <milevy1@gmail.com>
2019-07-22 19:39:31 -0600

    Adds POST /api/v1/trip_legs endpoint

--- spec/requests/api/v1/trip_legs/post_trip_legs_request_spec.rb
+++ spec/requests/api/v1/trip_legs/post_trip_legs_request_spec.rb
@@ -16,6 +16,31 @@ describe 'Trip Legs API V1 requests', type: :request do
 
       expect(response).to be_successful
       expect(response.status).to eq(201)
+
+      expect(trip.trip_legs.count).to eq(1)
+      trip_leg = trip.trip_legs.first
+
+      expect(trip_leg.distance).to eq(100)
+      expect(trip_leg.duration).to eq(200.0)
+      expect(trip_leg.sequence_number).to eq(1)
+    end
+
+    it 'Sad Path - invalid token' do
+      trip = Trip.create
+
+      valid_params = {
+        token: 'invalid token',
+        distance: 100,
+        duration: 200.0,
+        sequence_number: 1
+      }
+
+      post '/api/v1/trip_legs', params: valid_params
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      expect(trip.trip_legs.count).to eq(0)
     end
   end
 end
