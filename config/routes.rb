0eaff1e52b80fcb640a15c8da4d7e7b739ef3760
Matt Levy <milevy1@gmail.com>
2019-07-22 18:37:23 -0600

    Adds PATCH /api/v1/trips endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :trips, only: [:create, :index]
+      patch "/trips", to: 'trips#update'
     end
   end
 end
