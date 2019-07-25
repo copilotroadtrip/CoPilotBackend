27923c87590144ce19fb3533ca061472b1a8fe7a
Matt Levy <milevy1@gmail.com>
2019-07-25 07:26:18 -0600

    Adds patch to convert trip_leg distance to miles as a String

--- app/models/trip_leg.rb
+++ app/models/trip_leg.rb
@@ -6,8 +6,20 @@ class TripLeg < ApplicationRecord
     {
       'id':                 self.id,
       'sequence_number':    self.sequence_number,
-      'distance':           self.distance,
+      'distance':           distance_patch,
       'duration_in_hours':  self.duration,
     }
+  end
+
+  # Converts to mileage due to inconsistency
+  # poi-microservice is serving distance up as an integer `meters`
+  # this will catch that and convert to miles
+  def distance_patch
+    if self.distance.class == String # Already formatted as miles
+      return self.distance
+    else
+      miles = ( self.distance / 1609.34 ).round # 1609.34 meters per mile
+      return miles.to_s + ' mi'
+    end
   end
 end
